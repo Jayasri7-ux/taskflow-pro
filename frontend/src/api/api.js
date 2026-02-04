e53066557581727d87b85f83a951e59dd7b08125
Jaya Sri <jayasri@gmail.com>
2026-02-04 10:58:25 +0530

    Fix Production API URL switch

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "/api",
+    baseURL:
+        import.meta.env.MODE === "development"
+            ? "http://localhost:5000/api"
+            : "https://taskflow-backend-by15.onrender.com/api",
     withCredentials: true,
 });
 
